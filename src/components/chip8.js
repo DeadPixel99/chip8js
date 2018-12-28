dc5754aa0746f19b1eb6921db50a56207d9a56d4
Tony S <antonnnnn13@gmail.com>
2018-12-28 12:51:12 +0200

    Init

---
+++ src/components/chip8.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class Chip8 extends Component{
+
+    render() {
+        return (
+            <div className='chip8'>
+
+            </div>
+        );
+    }
+}
+
+export default Chip8;

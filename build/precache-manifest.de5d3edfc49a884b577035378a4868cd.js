ba0b74467f53cc5237c936236f821e43d630ea99
Tony S <antonnnnn13@gmail.com>
2019-03-24 11:29:36 +0200

    Added build root to git

---
+++ build/precache-manifest.de5d3edfc49a884b577035378a4868cd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e09cd8480ca54c97d8c4",
+    "url": "./static/css/main.9f630203.chunk.css"
+  },
+  {
+    "revision": "e09cd8480ca54c97d8c4",
+    "url": "./static/js/main.e09cd848.chunk.js"
+  },
+  {
+    "revision": "9eb600ee07a27cdad64f",
+    "url": "./static/js/runtime~main.9eb600ee.js"
+  },
+  {
+    "revision": "fc945d8e978ee1abf8b7",
+    "url": "./static/js/2.fc945d8e.chunk.js"
+  },
+  {
+    "revision": "896c9121069dcbed08f8929e55d61ce8",
+    "url": "./index.html"
+  }
+];
